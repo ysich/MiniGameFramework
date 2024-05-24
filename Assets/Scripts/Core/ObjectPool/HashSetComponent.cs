4530f6d7ab41edc2c9498dbbaa9505f03a4cb94b
叶偲诚 <ysich@qq.com>
2024-05-24 09:31:20 +0800

    feat: 对象池扩展

---
+++ Assets/Scripts/Core/ObjectPool/HashSetComponent.cs
@@ -0,0 +1,23 @@
+﻿/*---------------------------------------------------------------------------------------
+-- 负责人: onemt
+-- 创建时间: 2024-05-24 09:22:35
+-- 概述:
+---------------------------------------------------------------------------------------*/
+
+using System;
+using System.Collections.Generic;
+
+namespace Core
+{
+    public class HashSetComponent<T>:HashSet<T>,IDisposable
+    {
+        public static HashSetComponent<T> Create()
+        {
+            return ObjectPool.Instance.Fetch(typeof(HashSetComponent<T>)) as HashSetComponent<T>;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
