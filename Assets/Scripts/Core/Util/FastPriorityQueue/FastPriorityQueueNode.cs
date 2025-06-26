72fa302f12a903b16126051023cc8dfe192acabf
yesicheng <yesicheng@onemt.com.cn>
2025-06-26 17:49:16 +0800

    feat:FastPriorityQueue

---
+++ Assets/Scripts/Core/Util/FastPriorityQueue/FastPriorityQueueNode.cs
@@ -0,0 +1,18 @@
+﻿/*---------------------------------------------------------------------------------------
+-- 负责人: onemt
+-- 创建时间: 2025-06-26 17:34:33
+-- 概述:
+---------------------------------------------------------------------------------------*/
+
+namespace Core.FastPriorityQueue
+{
+    public class FastPriorityQueueNode
+    {
+        public float Priority { get; protected internal set; }
+        public int QueueIndex { get; internal set; }
+        
+#if DEBUG
+        public object Queue { get; internal set; }
+#endif
+    }
+}
