5139941035e64f6e17e1b1d772591aa48760d83b
unknown <yecaicheng@xmfunny.com>
2025-07-04 16:05:28 +0800

    feat:UIFramework

--- Assets/Scripts/Core/Module/UI/UIModule.cs
+++ Assets/Scripts/Core/Module/UI/UIModule.cs
@@ -11,7 +11,7 @@ namespace Core.Module.UI
         
         public UIManager Manager => uiManager;
         
-        public void Awake()
+        public void Awake() 
         {
             uiManager = UIManager.Instance;
         }
